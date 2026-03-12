129c08b165b77447f420a2efff9b9ea643fc67f7
Victor Ibironke <victoribironke2019@gmail.com>
2026-03-12 13:28:48 +0100

    feat: configure firebase credentials

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,10 +1,10 @@
 export const CREDENTIALS = {
   firebase: {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
+    apiKey: "AIzaSyDIJKNJWVySg7DOSAphkj5Fe_hdfIzLyho",
+    authDomain: "babcock-votes.firebaseapp.com",
+    projectId: "babcock-votes",
+    storageBucket: "babcock-votes.firebasestorage.app",
+    messagingSenderId: "386049332302",
+    appId: "1:386049332302:web:d037d90c8303a58daa1f1f",
   },
 };

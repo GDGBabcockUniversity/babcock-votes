8377e7fbf001333e4c1f60ddce0e14f48e51f60f
Victor Ibironke <victoribironke2019@gmail.com>
2026-03-21 18:29:32 +0100

    feat(utils): add helper functions for department name and ID lookup

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,13 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { DEPARTMENTS } from "./constants";
 
 export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
+
+export const getDepartmentName = (id: string) => {
+  return DEPARTMENTS.find((d) => d.id === id)?.name || id;
+};
+
+export const getDepartmentId = (name: string) => {
+  return DEPARTMENTS.find((d) => d.name === name)?.id || name;
+};

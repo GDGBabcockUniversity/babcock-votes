6c2c5486dede3f10645fc5c1dd129fa3e0573891
Victor Ibironke <victoribironke2019@gmail.com>
2026-03-26 11:04:30 +0100

    feat: add Google Auth provider

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,14 +1,13 @@
 import { initializeApp, getApps } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { CREDENTIALS } from "./constants";
 
 const app =
-  getApps().length === 0
-    ? initializeApp(CREDENTIALS.firebase)
-    : getApps()[0];
+  getApps().length === 0 ? initializeApp(CREDENTIALS.firebase) : getApps()[0];
 
 export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
 export const db = getFirestore(app);
 export const storage = getStorage(app);

6f1afdfdb4b8d5cac4341685647d3e9b16903168
Victor Ibironke <victoribironke2019@gmail.com>
2026-03-27 23:54:33 +0100

    feat: remove rounded corners from skeleton loader components

--- components/skeleton-loader.tsx
+++ components/skeleton-loader.tsx
@@ -14,7 +14,7 @@ export const SkeletonLoader = ({
 
 export const ElectionCardSkeleton = () => {
   return (
-    <div className="flex flex-col gap-3 rounded-xl border border-border bg-white p-4">
+    <div className="flex flex-col gap-3 border border-border bg-white p-4">
       <div className="flex items-center justify-between">
         <SkeletonLoader className="h-5 w-24 rounded-full" />
         <SkeletonLoader className="h-4 w-16" />
@@ -29,7 +29,7 @@ export const ElectionCardSkeleton = () => {
 
 export const CandidateCardSkeleton = () => {
   return (
-    <div className="flex flex-col items-center rounded-xl border border-border bg-white p-4 text-center">
+    <div className="flex flex-col items-center border border-border bg-white p-4 text-center">
       <SkeletonLoader className="size-20 rounded-full mb-3" />
       <SkeletonLoader className="h-5 w-3/4 mb-1" />
       <SkeletonLoader className="h-3 w-1/2" />
